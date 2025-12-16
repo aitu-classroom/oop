375ee3c5b52853457e7f3c681c68df62aa8e79b6
Konstantin Latuta <konstantin.latuta@gmail.com>
2025-12-16 15:29:50 +0500

    Update Hello.java

--- week01/Hello.java
+++ week01/Hello.java
@@ -1,4 +1,5 @@
-// In this example, data and the method that operates on the data are encapsulated inside Student class
+// In this example, data and the methods that operate on the data are encapsulated inside Student class
+// and access to the data is controlled via getters and setters.
 // rename the file to Hello.java before you run it
 
 public class Hello {
@@ -8,6 +9,8 @@ public class Hello {
         Student alice = new Student("Alice", 56789, 4.0);
         Student bob = new Student("Robert", 54321, 2.9);
         Student john = new Student("Muhammedjohn", 12345, 3.5);
+
+        john.setGpa(4.0);
         
         alice.print();
         bob.print();
@@ -18,9 +21,9 @@ public class Hello {
 // data: name, id, gpa
 // methods: print(), Student()
 class Student{
-    String name;
-    int id;
-    double gpa;
+    private String name;
+    private int id;
+    private double gpa;
 
     public Student(String name, int id, double gpa) {
         this.name = name;
@@ -28,7 +31,16 @@ class Student{
         this.gpa = gpa;
     }
 
-    void print(){
+    public void print(){
         System.out.println("{name: "+name+", id: "+id+", gpa: "+gpa+"}");
     }
+
+    public String getName(){ return name; }
+    public void setName(String name){ this.name = name; }
+
+    public int getId(){ return id; }
+    public void setId(int id){ this.id = id; }
+
+    public double getGpa(){ return gpa; }
+    public void setGpa(double gpa){ this.gpa = gpa; }
 }
